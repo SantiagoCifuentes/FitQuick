30151c1e946cb68c98ea95f18b149c80c099a0af
Esneyder <brinezgomezesneyder20@gmail.com>
2025-04-01 15:39:08 -0500

    Esneyder - Actualizacion
    Agregue logo de interfaz y general en app.
    Actualización de nombre del main.
    Ajustes de interfaz y corrección de errores.

--- app/src/main/java/com/example/fitquick/MainActivity.kt
+++ app/src/main/java/com/example/fitquick/MainActivity.kt
@@ -2,25 +2,15 @@ package com.example.fitquick
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.fitquick.ui.theme.FitQuickTheme
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.principal)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNavigationView.setOnItemSelectedListener{ item ->
